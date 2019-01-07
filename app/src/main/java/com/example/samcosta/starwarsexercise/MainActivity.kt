f213795ba8f446df6b10bfeac6b4d38611cb5391
Sam <samuel.dacosta@student.manchester.ac.uk>
2019-01-07 20:37:13 +0000

    Initial Commit

---
+++ app/src/main/java/com/example/samcosta/starwarsexercise/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.samcosta.starwarsexercise
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/samcosta/starwarsexercise/engine/api/ApiCallback.kt
@@ -0,0 +1,67 @@
+package com.example.samcosta.starwarsexercise.engine.api
+
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+/**
+ * Created by samcosta on 07/01/2019.
+ *
+ *
+ * Disclaimer: I have not written this file from scratch for this exercise, it's modified from a version
+ * I've used on another Kotlin project
+ */
+
+typealias ApiCallCompletion<T> = (ApiResponse<T>) -> Unit
+
+private fun parseError(response: Response<*>?): CallFailure.ApiError? {
+    val error = response?.errorBody()?.string() ?: return null
+
+    return try {
+        CallFailure.ApiError(response.code(), error)
+    } catch (ignored: Throwable) {
+        null
+    }
+}
+
+fun <T>Call<T>.enqueue(completion: ApiCallCompletion<T>) {
+    this.enqueue(object : Callback<T> {
+        override fun onFailure(call: Call<T>?, t: Throwable?) {
+            completion(ApiResponse.failure<T>(t))
+        }
+
+        override fun onResponse(call: Call<T>?, response: Response<T>?) {
+            val isSuccess = response?.isSuccessful ?: false
+
+            val result = if (isSuccess){
+                ApiResponse.success(response?.body())
+            } else {
+                ApiResponse.error(parseError(response))
+            }
+
+            completion(result)
+        }
+    })
+}
+
+data class ApiResponse<T>(val isSuccess: Boolean, val body: T?, val errorBody: CallFailure?) {
+
+    fun <S>convert(): ApiResponse<S> {
+        if (this.isSuccess) {
+            throw IllegalArgumentException("Can only convert error responses")
+        }
+
+        return ApiResponse(false, null, this.errorBody)
+    }
+
+    companion object {
+        fun <T> failure(throwable: Throwable?): ApiResponse<T> = ApiResponse(false, null, CallFailure.ApiFailure(throwable))
+        fun <T> success(body: T?): ApiResponse<T> = ApiResponse(true, body, null)
+        fun <T> error(error: CallFailure.ApiError?): ApiResponse<T> = ApiResponse(false, null, error)
+    }
+}
+
+sealed class CallFailure {
+    data class ApiError(val errorCode: Int, val rawMessage: String): CallFailure()
+    data class ApiFailure(val throwable: Throwable?): CallFailure()
+}
